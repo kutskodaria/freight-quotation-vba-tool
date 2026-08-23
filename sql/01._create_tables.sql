2d3ff705f3af4ef7833044ff0cfc3b0cb290794b
Дарья Кутько <kutskodaria@gmail.com>
2026-08-23 23:20:11 +0300

    Delete sql directory

--- sql/01._create_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS china_belarus_logistics;
-USE china_belarus_logistics;
-
-CREATE TABLE IF NOT EXISTS freight_tariffs (
-    tariff_id INT AUTO_INCREMENT PRIMARY KEY,
-    route_name VARCHAR(100) NOT NULL,
-    container_type VARCHAR(20) NOT NULL,
-    base_cost_usd DECIMAL(10, 2) NOT NULL
-);
